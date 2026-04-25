158c33236693e665426db59dfdc23a9f2913eaa9
ErikRidhoFirm <erikridho1@gmail.com>
2026-04-25 11:29:06 +0700

    Menyelesaikan langkah-langkah 6 (custom document dan error)

--- Jobsheet 6/my-app/src/components/layouts/AppShell/index.tsx
+++ Jobsheet 6/my-app/src/components/layouts/AppShell/index.tsx
@@ -1,8 +1,7 @@
 import Navbar from "../navbar";
-import Footer from "../footer";
 import { useRouter } from "next/router";
 
-const disableNavbar = ['auth/login', '/auth/register'];
+const disableNavbar = ['/auth/login', '/auth/register', '/404'];
 
 type AppShellProps = {
   children: React.ReactNode;
@@ -11,8 +10,8 @@ type AppShellProps = {
 const AppShell = (props: AppShellProps) => {
   const { children } = props;
   const {pathname} = useRouter();
-  const router = useRouter();
-  console.log(router);
+  // const router = useRouter();
+  // console.log(router);
   return (
     <main>
       {!disableNavbar.includes(pathname) && <Navbar />}
