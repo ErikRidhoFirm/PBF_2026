1bddbd8fccf681d9079d6d85b3becfaac476fe86
ErikRidhoFirm <erikridho1@gmail.com>
2026-04-25 12:20:08 +0700

    menyelesaikan soal nomer 1 dari jobsheet 6

--- Jobsheet 6/my-app/src/pages/404.tsx
+++ Jobsheet 6/my-app/src/pages/404.tsx
@@ -3,9 +3,13 @@ import styles from "@/styles/404.module.scss";
 const Custom404 = () => {
     return (
         <div className={styles.error}>
+            <head>
+                <title>404 - Halaman Tidak Ditemukan</title>
+            </head>
             <img src="/page-not-found.png" alt="404" className={styles.error_image} />
             <h1>404 - Halaman Tidak Ditemukan</h1>
             <p>Maaf, halaman yang anda cari tidak ada.</p>
+            <p>Halaman ini digunakan untuk menampilkaan gambar, pesan ketika halaman yang dituju tidak ditemukan</p>
         </div>
     );
 };
