312b5e610b199a526164cc5441c017fec91ea9ca
ErikRidhoFirm <erikridho1@gmail.com>
2026-04-27 21:19:29 +0700

    perbaikan nama file di pages/produk

--- Jobsheet8/my-app/src/pages/produk/index.tsx
+++ Jobsheet8/my-app/src/pages/produk/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import TampilanProduk from "@/pages/views/produk";
+import TampilanProduk from "../views/produk";
 import useSWR from "swr";
 import fetcher from "../utils/db/swr/fetcher";
 
