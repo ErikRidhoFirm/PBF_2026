a8b0717471307b3aa68f9658391409c96be63f21
ErikRidhoFirm <erikridho1@gmail.com>
2026-04-28 16:42:09 +0700

    menambahkan jobsheet 10

---
+++ Jobsheet10/my-app/src/pages/produk/index.tsx
@@ -0,0 +1,30 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import TampilanProduk from "../views/produk";
+import useSWR from "swr";
+import fetcher from "../utils/db/swr/fetcher";
+
+// const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+const kategori = () => {
+  // const [isLogin, setIsLogin] = useState(false);
+  // const { push } = useRouter();
+  const [products, setProducts] = useState([]);
+
+  const { data, error, isLoading } = useSWR("/api/produk", fetcher);
+  // cek apakah data, error, dan isLoading sudah benar
+  
+  // useEffect(() => {
+  // if (!isLogin) {
+  //   PushManager("/auth/login");
+  // }
+  // },[]);
+
+  return (
+    <div>
+       <TampilanProduk products={isLoading? []: data.data} />
+    </div>
+  );
+};
+
+export default kategori;
