e532ef18216ec85e531e023b297056fcbb36f04d
ErikRidhoFirm <erikridho1@gmail.com>
2026-04-29 07:14:16 +0700

    menambahkan jobsheet 11

---
+++ Jobsheet11/my-app/src/pages/produk/[id].tsx
@@ -0,0 +1,22 @@
+import {useRouter} from 'next/router';
+import useSWR from 'swr';
+import fetcher from '../../utils/db/swr/fetcher';
+import TampilanProduk from '../../views/produk';
+
+const halamanProduk = () => {
+    // const Router = useRouter();
+    // console.log(Router);
+    const {query} = useRouter();
+    const { data, error, isLoading } = useSWR (
+        query.id ? `/api/produk?kategori=${query.id}` : null,
+        fetcher
+    )
+    return (
+        <div>
+            <h1>Halaman Produk</h1>
+            <TampilanProduk products={isLoading ? [] : data?.data || []}/>
+        </div>
+    );
+};
+
+export default halamanProduk;
